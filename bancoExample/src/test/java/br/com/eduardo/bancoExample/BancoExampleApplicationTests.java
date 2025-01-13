03dcf7768325011e1503fbba814295523348d95c
EduardoMRT <eduksoficial@gmail.com>
2025-01-13 08:21:13 -0300

    Project Structure

---
+++ bancoExample/src/test/java/br/com/eduardo/bancoExample/BancoExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduardo.bancoExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
